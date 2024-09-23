bd49b336003e8207b944d1471094082cc417aef7
Ashis Kumar Joy <ashis263@gmail.com>
2024-09-23 16:32:46 +0600

    added header buttons interactive with javascript

--- js/main.js
+++ js/main.js
@@ -6,13 +6,19 @@ document.getElementById('blog-btn').
 const donationBtn = document.getElementById('donation-btn');
 const historyBtn = document.getElementById('history-btn');
 
+const donationSection = document.getElementById('donation');
+const historySection = document.getElementById('history');
+
 donationBtn.addEventListener('click', function(){
     historyBtn.classList.add('btn-outline');        
     historyBtn.classList.add('text-textSecondary');        
     historyBtn.classList.remove('bg-primary');        
     donationBtn.classList.remove('btn-outline');        
     donationBtn.classList.remove('text-textSecondary');        
-    donationBtn.classList.add('bg-primary');        
+    donationBtn.classList.add('bg-primary');
+    donationSection.classList.remove('hidden'); 
+    historySection.classList.add('hidden'); 
+    
 });
 
 historyBtn.addEventListener('click', function(){
@@ -22,4 +28,8 @@ historyBtn.addEventListener('click', function(){
     historyBtn.classList.remove('btn-outline');        
     historyBtn.classList.remove('text-textSecondary');        
     historyBtn.classList.add('bg-primary');        
+    donationSection.classList.add('hidden'); 
+    historySection.classList.remove('hidden'); 
+    donationSection.classList.add('hidden'); 
 });
+
