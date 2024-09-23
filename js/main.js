a89027a5c309622fa78aeb46844a62a89d5f8029
Ashis Kumar Joy <ashis263@gmail.com>
2024-09-23 13:58:10 +0600

    fixed header buttons

--- js/main.js
+++ js/main.js
@@ -7,19 +7,19 @@ const donationBtn = document.getElementById('donation-btn');
 const historyBtn = document.getElementById('history-btn');
 
 donationBtn.addEventListener('click', function(){
-    historyBtn.classList.remove('btn-outline');        
-    historyBtn.classList.remove('text-textSecondary');        
-    historyBtn.classList.add('bg-primary');        
-    donationBtn.classList.add('btn-outline');        
-    donationBtn.classList.add('text-textSecondary');        
-    donationBtn.classList.remove('bg-primary');        
-});
-
-historyBtn.addEventListener('click', function(){
-    donationBtn.classList.remove('btn-outline');        
-    donationBtn.classList.remove('text-textSecondary');        
-    donationBtn.classList.add('bg-primary');        
     historyBtn.classList.add('btn-outline');        
     historyBtn.classList.add('text-textSecondary');        
     historyBtn.classList.remove('bg-primary');        
+    donationBtn.classList.remove('btn-outline');        
+    donationBtn.classList.remove('text-textSecondary');        
+    donationBtn.classList.add('bg-primary');        
+});
+
+historyBtn.addEventListener('click', function(){
+    donationBtn.classList.add('btn-outline');        
+    donationBtn.classList.add('text-textSecondary');        
+    donationBtn.classList.remove('bg-primary');        
+    historyBtn.classList.remove('btn-outline');        
+    historyBtn.classList.remove('text-textSecondary');        
+    historyBtn.classList.add('bg-primary');        
 });
